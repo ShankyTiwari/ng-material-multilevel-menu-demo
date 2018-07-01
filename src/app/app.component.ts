6f84bcfce8eafec2a609d5e6c5ee83733a9b85ed
ShankyTiwari <shanshank69@gmail.com>
2018-07-01 12:12:39 +0530

    Added new demos for newer version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,74 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  appitems = [
+  selectedData = null;
+  selectedData2 = null;
+  selectedData3 = null;
+
+  appItems = [
+    {
+      label: 'Item 1 (with Font awesome icon)',
+      faIcon: 'fab fa-500px',
+      items: [
+        {
+          label: 'Item 1.1',
+          link: '/item-1-1',
+          faIcon: 'fab fa-accusoft'
+        },
+        {
+          label: 'Item 1.2',
+          faIcon: 'fab fa-accessible-icon',
+          items: [
+            {
+              label: 'Item 1.2.1',
+              link: '/item-1-2-1',
+              faIcon: 'fas fa-allergies'
+            },
+            {
+              label: 'Item 1.2.2',
+              faIcon: 'fas fa-ambulance',
+              items: [
+                {
+                  label: 'Item 1.2.2.1',
+                  link: 'item-1-2-2-1',
+                  faIcon: 'fas fa-anchor'
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    },
+    {
+      label: 'Item 2',
+      icon: 'alarm',
+      items: [
+        {
+          label: 'Item 2.1',
+          link: '/item-2-1',
+          icon: 'favorite'
+        },
+        {
+          label: 'Item 2.2',
+          link: '/item-2-2',
+          icon: 'favorite_border'
+        }
+      ]
+    },
+    {
+      label: 'Item 3',
+      link: '/item-3',
+      icon: 'offline_pin'
+    },
+    {
+      label: 'Item 4',
+      link: '/item-4',
+      icon: 'star_rate',
+      hidden: true
+    }
+  ];
+
+  appItems2 = [
     {
       label: 'Item 1 with Font awesome icon',
       faIcon: 'fab fa-500px',
@@ -69,12 +136,97 @@ export class AppComponent {
     }
   ];
 
-  config = {
+
+  appItems3 = [
+    {
+      label: 'Item 1 with Font awesome icon',
+      faIcon: 'fab fa-500px',
+      items: [
+        {
+          label: 'Item 1.1',
+          link: '/item-1-1',
+          faIcon: 'fab fa-accusoft'
+        },
+        {
+          label: 'Item 1.2',
+          faIcon: 'fab fa-accessible-icon',
+          items: [
+            {
+              label: 'Item 1.2.1',
+              link: '/item-1-2-1',
+              faIcon: 'fas fa-allergies'
+            },
+            {
+              label: 'Item 1.2.2',
+              faIcon: 'fas fa-ambulance',
+              items: [
+                {
+                  label: 'Item 1.2.2.1',
+                  link: 'item-1-2-2-1',
+                  faIcon: 'fas fa-anchor'
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    },
+    {
+      label: 'Item 2',
+      icon: 'alarm',
+      items: [
+        {
+          label: 'Item 2.1',
+          link: '/item-2-1',
+          icon: 'favorite'
+        },
+        {
+          label: 'Item 2.2',
+          link: '/item-2-2',
+          icon: 'favorite_border'
+        }
+      ]
+    },
+    {
+      label: 'Item 3',
+      link: '/item-3',
+      icon: 'offline_pin'
+    },
+    {
+      label: 'Item 4',
+      link: '/item-4',
+      icon: 'star_rate',
+      hidden: true
+    }
+  ];
+
+  config2 = {
     paddingAtStart: true,
-    classname: 'my-custom-class'
+    classname: 'my-custom-class',
+    listBackgroundColor: `rgb(208, 241, 239)`,
+    fontColor: `rgb(8, 54, 71)`,
+    backgroundColor: `rgb(208, 241, 239)`,
+    selectedListFontColor: `red`,
   };
 
-  selectedItem($event) {
-    console.log($event);
+  config3 = {
+    paddingAtStart: false,
+    classname: 'my-custom-list',
+    listBackgroundColor: `rgb(120, 120, 120)`,
+    fontColor: `#a5a5a5`,
+    backgroundColor: `rgb(120, 120, 120)`,
+    selectedListFontColor: `#fff`,
+  };
+
+  selectedItem(selectedData) {
+    this.selectedData = selectedData;
+  }
+
+  selectedItem2(selectedData) {
+    this.selectedData2 = selectedData;
+  }
+
+  selectedItem3(selectedData) {
+    this.selectedData3 = selectedData;
   }
 }
